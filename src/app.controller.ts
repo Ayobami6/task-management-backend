89c496c4ca21b8c776b76d92d3dcfb719c1624b9
Ayobami <ayobamidele006@gmail.com>
2023-11-11 07:50:30 +0100

    add task module&install packages

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+// Controller handle request and response over http protocols, Uses dependency injection from service
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TasksModule } from './tasks/tasks.module';
+
+/**
+ * Main App Module, Entry point for all other modules
+ * Module a Singleton which means they can be imported by other modules
+ *  Modules Property: Providers, Controllers, Exports, Import
+ */
+
+// Providers: array of services to be provided to the module via dependency injection
+// Controllers: arrays of controllers to be instantiated within the project
+// Exports: arrays of service/providers to be exported to modules that import the particular module
+// import: arrays of modules to be used by the modules
 
 @Module({
-  imports: [],
+  imports: [TasksModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }

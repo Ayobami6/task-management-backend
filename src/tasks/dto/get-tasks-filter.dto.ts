cb69190bdbb7d30277d3347dd956312e283f2a87
Ayobami Alaran A. <ayobamidele006@gmail.com>
2023-11-15 20:35:17 +0000

    feat: Add validation to GetTaskFilterDto
    - Added class-validator annotations to the `search` and `status`
    properties in the `GetTaskFilterDto` class for input validation.

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,12 @@
+import { IsEnum, IsOptional, IsString } from 'class-validator';
 import { TaskStatus } from '../task.model';
 
 export class GetTaskFilterDto {
+  @IsOptional()
+  @IsString()
   search: string;
+
+  @IsOptional()
+  @IsEnum(TaskStatus)
   status: TaskStatus;
 }

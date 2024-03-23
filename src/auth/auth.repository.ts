bff0b7bc08434b3638130181b7965b1fc2fabb84
Ayobami6 <ayobamidele006@gmail.com>
2024-03-23 17:35:10 +0100

    Handle conflict username in users registration

--- src/auth/auth.repository.ts
+++ src/auth/auth.repository.ts
@@ -1,6 +1,11 @@
 import { User } from './auth.entity';
 import { appDatasource } from '../tasks/app.datasource';
 import { AuthCredentialsDto } from './dto/auth.credentials.dto';
+import {
+  HttpException,
+  HttpStatus,
+  InternalServerErrorException,
+} from '@nestjs/common';
 
 export const authRepository = appDatasource.getRepository(User).extend({
   async createUser(createUserDto: AuthCredentialsDto): Promise<User> {
@@ -9,7 +14,18 @@ export const authRepository = appDatasource.getRepository(User).extend({
       username,
       password,
     });
-    await this.save(user);
-    return user;
+    try {
+      await this.save(user);
+      return user;
+    } catch (error) {
+      if (error.code == 23505) {
+        throw new HttpException(
+          'Username already exists',
+          HttpStatus.BAD_REQUEST,
+        );
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   },
 });
--- src/auth/dto/auth.credentials.dto.ts
+++ src/auth/dto/auth.credentials.dto.ts
@@ -1,4 +1,4 @@
-import { IsString, MaxLength, MinLength } from 'class-validator';
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
 
 export class AuthCredentialsDto {
   @MinLength(6)
@@ -8,5 +8,9 @@ export class AuthCredentialsDto {
   @MinLength(8)
   @MaxLength(20)
   @IsString()
+  @Matches(
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+    { message: 'Password is too weak' },
+  )
   password: string;
 }

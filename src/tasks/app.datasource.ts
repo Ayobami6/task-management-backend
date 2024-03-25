4cb5a9eca2ba3f400f8ef29e1a1be09ea4a569d5
Ayobami6 <ayobamidele006@gmail.com>
2024-03-25 23:10:12 +0100

    Update prod db

--- src/tasks/app.datasource.ts
+++ src/tasks/app.datasource.ts
@@ -2,11 +2,11 @@ import { DataSource } from 'typeorm';
 
 export const appDatasource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'ayo',
-  password: 'haywon',
-  database: 'nest_tmb',
+  host: 'monorail.proxy.rlwy.net',
+  port: 41821,
+  username: 'postgres',
+  password: 'JoRKEhNurDjMxrtmfKSahUeHHIFITgyT',
+  database: 'railway',
   synchronize: true,
   migrationsRun: false,
   entities: [`${__dirname}/../**/**.entity{.ts,.js}`],
